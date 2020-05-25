44edc57b5ea5564220cf1d249821d2d88037c007
aleksanderbastek <officialbastek@gmail.com>
2020-05-25 12:38:42 +0200

    dodanie paska wyszukiwania

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -5,9 +5,19 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SearchComponent } from "./search/search.component";
 
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
 @NgModule({
   declarations: [AppComponent, SearchComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
