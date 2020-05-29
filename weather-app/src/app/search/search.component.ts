fb91f81dee9fba914acca14fa1b4900ea8b70620
aleksanderbastek <officialbastek@gmail.com>
2020-05-29 11:53:06 +0200

    użycie get dla miasta

--- weather-app/src/app/search/search.component.ts
+++ weather-app/src/app/search/search.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../api.service";
+
+import { city } from "../city";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
 })
 export class SearchComponent implements OnInit {
+  weather;
 
-  constructor() { }
+  constructor(private api: ApiService) {}
 
-  ngOnInit(): void {
+  getWeather(name: string): void {
+    this.weather = this.api.getWeather(name).subscribe((data) => {
+      console.log(data);
+      this.weather = data;
+    });
   }
 
+  ngOnInit(): void {}
 }
