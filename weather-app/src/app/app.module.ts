d0dede890abcf513d16d0647d497c7e7187eca6a
aleksanderbastek <officialbastek@gmail.com>
2020-06-02 09:25:38 +0200

    poprawka BrowserAnimationsModule

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -20,6 +21,7 @@ import { ApiService } from "./api.service";
     MatButtonModule,
     MatIconModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [ApiService],
   bootstrap: [AppComponent],
